e9eeae6ef480ee23b6ed8ad67151bb66b6fc54d2
princelit <mervynk020@gmail.com>
2025-11-11 10:07:12 +0300

    new

--- src/when.kt
+++ src/when.kt
@@ -5,7 +5,14 @@ fun main() {
     println("Enter day (i.e 1, 2, 3, 4, 5) : ")
     var day = y.nextInt()
 
-    var result
+    var result = when(day){
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednesday"
+        4 -> "Thursday"
+        5 -> "Friday"
+        else -> "Invalid day of the week"
+    }
 
 
 
