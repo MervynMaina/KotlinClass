3390afac1c1cc4347b50b43bf21bb615d7c75e3c
princelit <mervynk020@gmail.com>
2025-11-11 09:13:53 +0300

    new

---
+++ src/operators.kt
@@ -0,0 +1,41 @@
+fun main() {
+    //Arithmetic Operators - Simple calculations
+    var x = 100
+    var y = 10
+
+    println(x + y)
+    println(x - y)
+    println(x * y)
+    println(x / y)
+    println(x % y) //Modulus - Returns remainder
+
+
+    //Comparison Operators - Returns boolean
+    var a = 89
+    var b = 56
+
+    println(a > b)
+    println(a < b)
+    println(a >= b)
+    println(a <= b)
+    println(a == b)
+    println(a != b)
+
+
+    //Assignment Operators - Assign values to variables
+    var z = 45
+    println(z)
+
+    z += 5
+    println(z)
+
+
+    //Logical Operators - &&, ||, !
+    var log = 78
+
+    println(log > 34 && log < 60)
+    println(log > 34 || log < 60)
+
+
+
+}
