6633b4a86f1d5c6446694271179e78539d73ea8e
princelit <mervynk020@gmail.com>
2025-11-12 10:04:14 +0300

    new

---
+++ src/functions.kt
@@ -0,0 +1,21 @@
+fun main() {
+    //Inbuilt Functions/Standard Library Functions - Already exist
+
+    var result = Math.sqrt(144.0)
+    println("The squareroot is $result")
+
+    var number = Math.round(54.81)
+    println("The output is $number")
+
+    day()//Calling a function
+}
+
+//User-Defined Functions
+fun day(){
+    println("Today is Wednesday")
+}
+
+//Parameters/Variables and Arguments/Values
+fun student(name: String, age: Int, course: String){
+
+}
