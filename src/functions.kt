75a20a0928218fe3c8833b230811ae22070176fb
princelit <mervynk020@gmail.com>
2025-11-12 10:14:01 +0300

    new

--- src/functions.kt
+++ src/functions.kt
@@ -7,7 +7,18 @@ fun main() {
     var number = Math.round(54.81)
     println("The output is $number")
 
+    println()
+
     day()//Calling a function
+
+    println()
+
+    student("Mark", 25, "MIT")
+    student("Ann", 23, "Cybersecurity")
+
+    println()
+
+    add(54, 87)
 }
 
 //User-Defined Functions
@@ -17,5 +28,11 @@ fun day(){
 
 //Parameters/Variables and Arguments/Values
 fun student(name: String, age: Int, course: String){
+    println("Name : $name, Age : $age, Course : $course")
+
+}
+
+fun add(num1: Int, num2: Int){
+    println(num1 + num2)
 
 }
